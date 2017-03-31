fe5bdfe5111569c9917ae2eaccf31d069223569b
Jack Herrington <jherrington@walmartlabs.com>
2017-03-31 14:51:12 -0700

    Defensive result cell renderer

--- client/components/result-cell.js
+++ client/components/result-cell.js
@@ -2,15 +2,20 @@ import React from 'react';
 import CheckMark from './check-mark';
 
 const ResultCell = (props) => (
-  <td className={`result ${props.result.status}`} style={{textAlign: "center"}}>
-    <a href={`/result/${props.resultId}/${props.environment}`}>
-      {props.result.status === 'pending' ? <i className="glyphicon glyphicon-minus"></i> : null}
-      {props.result.status === 'started' ? <img src="/spin.gif" style={{width: 25, height: 25}}></img> : null}
-      {props.result.status === 'pass' ? <CheckMark retryCount={props.result.retryCount ? props.result.retryCount : 0} /> : null}
-      {props.result.status === 'fail' ? <i className="glyphicon glyphicon-remove"></i> : null}
-      {props.result.status === 'retry' ? <i className="glyphicon glyphicon-refresh"></i> : null}
-    </a>
-  </td>
+  props.result ? (
+    <td className={`result ${props.result.status}`} style={{textAlign: "center"}}>
+      <a href={`/result/${props.resultId}/${props.environment}`}>
+        {props.result.status === 'pending' ? <i className="glyphicon glyphicon-minus"></i> : null}
+        {props.result.status === 'started' ? <img src="/spin.gif" style={{width: 25, height: 25}}></img> : null}
+        {props.result.status === 'pass' ? <CheckMark retryCount={props.result.retryCount ? props.result.retryCount : 0} /> : null}
+        {props.result.status === 'fail' ? <i className="glyphicon glyphicon-remove"></i> : null}
+        {props.result.status === 'retry' ? <i className="glyphicon glyphicon-refresh"></i> : null}
+      </a>
+    </td>
+  ) : (
+    <td>
+    </td>
+  )
 );
 
 export default ResultCell;
